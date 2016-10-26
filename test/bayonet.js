0e087ad626a9760b59df0e4166139a7ff1087dfd
Luis Herrada <luisehk@gmail.com>
2016-10-26 09:10:23 -0500

    Make api version mandatory, better fixtures and consulting api unit tests

--- test/bayonet.js
+++ test/bayonet.js
@@ -9,20 +9,60 @@ var chai = require('chai'),
 
 describe('Bayonet SDK', function () {
     describe('#configure(options)', function () {
+        it('api version should be mandatory', function () {
+            expect(function () {
+                bayonet.configure();
+            }).to.throw(Error);
+        });
+
         it('should not return anything', function () {
-            expect(bayonet.configure()).equal(undefined);
+            expect(bayonet.configure({version: 1})).equal(undefined);
         });
 
         it('should set api_key', function () {
             var api_key = fx.requests.invalid_token;
 
             bayonet.configure({
+                version: 1,
                 api_key: api_key
             });
 
             expect(
                 bayonet.api.getConfig().api_key
             ).equal(api_key);
+        });
+    });
+
+    describe('#api.consulting', function () {
+        it('should validate api key', function () {
+            bayonet.configure({
+                version: 1,
+                api_key: fx.requests.invalid_token
+            });
+
+            return bayonet.api.consulting(
+                fx.requests.consulting
+            ).then(function (r) {
+                expect(true).to.equal(false);
+            }).catch(function (r) {
+                expect(r.error.reason_code).to.equal('11');
+            });
+        });
+
+        it('should return feedback_api_trans_code', function () {
+            bayonet.configure({
+                version: 1,
+                api_key: process.env.BAYONET_API_KEY
+            });
+
+            return bayonet.api.consulting(
+                fx.requests.consulting
+            ).then(function (r) {
+                expect(r.reason_code).to.equal('00');
+                expect(r).to.have.property('feedback_api_trans_code');
+            }).catch(function (r) {
+                expect(true).to.equal(false);
+            });
         });
     });
 });
